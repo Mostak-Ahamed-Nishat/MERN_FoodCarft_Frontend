8792634fb3328e43f11d77c4e21d60bae902699d
Mostak-Ahamed-Nishat <mostakahamed484@gmail.com>
2024-05-13 00:56:31 +0600

    Auth0 authentication added

---
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,43 @@
+import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+
+
+type Props = {
+  children: React.ReactNode;
+};
+
+function Auth0ProviderWithNavigate({ children }: Props) {
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URI;
+
+  if (!domain || !clientId || !redirectUri) {
+    throw Error("Unable to initialize auth0. something missing");
+  }
+  console.log(domain)
+  console.log(clientId)
+  console.log(redirectUri)
+
+  // appState: store some data that we might need later.EX: current url that user on before we redirect to the login
+
+  const onTheTimeThatRedirectingCallback = (
+    appState?: AppState,
+    user?: User
+  ) => {
+    console.log(user);
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}
+      onRedirectCallback={onTheTimeThatRedirectingCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+}
+
+export default Auth0ProviderWithNavigate;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import footerLogo from "../../public/icon-b.png";
 
 export default function Footer() {
   return (
-    <div className=" bg-orange-500 py-2 ">
+    <div className=" bg-orange-500 py-5 ">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <span>
           <img src={footerLogo} alt="Food craft footer logo" className="h-10" />
