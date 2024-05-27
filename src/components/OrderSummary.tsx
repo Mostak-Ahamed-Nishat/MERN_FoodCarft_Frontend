9ed2581565f123759ccdfcb0403db12d8f04cff1
Mostak-Ahamed-Nishat <mostakahamed484@gmail.com>
2024-05-28 00:48:36 +0600

    Local changes adding

--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -11,62 +11,6 @@ type Props = {
   removeFromCart: (cartItem: CartItem) => void;
 };
 
-// export default function OrderSummary({
-//   restaurant,
-//   cartItems,
-//   removeFromCart,
-// }: Props) {
-//   const getTotalCost = () => {
-//     const totalInPence =
-//       cartItems?.reduce(
-//         (total, cartItem) => total + cartItem.price * cartItem.quantity,
-//         0
-//       ) || 0;
-
-//     const totalWithDelivery = totalInPence + (restaurant?.deliveryPrice || 0);
-
-//     return (totalWithDelivery / 100).toFixed(2);
-//   };
-
-//   return (
-//     <>
-//       <CardHeader>
-//         <CardTitle className="text-2xl font-bold tracking-tight flex justify-between">
-//           <span>Your Order</span>
-//           <span>£{getTotalCost()}</span>
-//         </CardTitle>
-//       </CardHeader>
-//       <CardContent className="flex flex-col gap-5">
-//         {cartItems?.map((item) => (
-//           <div key={item._id} className="flex justify-between">
-//             <span>
-//               <Badge variant="outline" className="mr-2">
-//                 {item.quantity}
-//               </Badge>
-//               {item.name}
-//             </span>
-//             <span className="flex items-center gap-1">
-//               <Trash
-//                 className="cursor-pointer"
-//                 color="red"
-//                 size={20}
-//                 onClick={() => removeFromCart(item)}
-//               />
-//               £{((item.price * item.quantity) / 100).toFixed(2)}
-//             </span>
-//           </div>
-//         ))}
-//         <Separator />
-//         <div className="flex justify-between">
-//           <span>Delivery</span>
-//           <span>£{(restaurant?.deliveryPrice / 100).toFixed(2)}</span>
-//         </div>
-//         <Separator />
-//       </CardContent>
-//     </>
-//   );
-// }
-
 export default function OrderSummary({
   restaurant,
   cartItems,
@@ -115,12 +59,7 @@ export default function OrderSummary({
         <Separator />
         <div className="flex justify-between">
           <span>Delivery</span>
-          <span>
-            £
-            {restaurant?.deliveryPrice
-              ? (restaurant.deliveryPrice / 100).toFixed(2)
-              : "0.00"}
-          </span>
+          <span>£{(restaurant?.deliveryPrice / 100).toFixed(2)}</span>
         </div>
         <Separator />
       </CardContent>
--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -14,198 +14,17 @@ import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 
 export type CartItem = {
-  _id: string | undefined;
+  _id: string;
   name: string;
   price: number;
   quantity: number;
 };
 
-// function DetailPage() {
-//   //Get the restaurantId from parameter
-//   const { restaurantId } = useParams();
-//   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
-
-//   const { createCheckoutSession, isLoading: isCheckoutLoading } =
-//     useCreateCheckoutSession();
-
-//   const [cartItems, setCartItems] = useState<CartItem[]>(() => {
-//     const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`);
-//     return storedCartItems ? JSON.parse(storedCartItems) : [];
-//   });
-
-//   if (isLoading) {
-//     return (
-//       <div className="flex justify-center text-center align-middle">
-//         <Loading />
-//       </div>
-//     );
-//   }
-
-//   // Add item to cart
-//   const addToCart = (menuItem: MenuItemType) => {
-//     setCartItems((prevCartItems) => {
-//       // Check if the item already exists in the cart
-//       const existingCartItem = prevCartItems.find(
-//         (cartItem) => cartItem._id === menuItem._id
-//       );
-//       let updatedCartItems;
-
-//       // If the item is already in the cart, increase the quantity
-//       if (existingCartItem) {
-//         updatedCartItems = prevCartItems.map((cartItem) =>
-//           cartItem._id === existingCartItem._id
-//             ? { ...cartItem, quantity: cartItem.quantity + 1 }
-//             : cartItem
-//         );
-//       } else {
-//         // If the item is not in the cart, add the item with quantity 1
-//         updatedCartItems = [
-//           ...prevCartItems,
-//           {
-//             _id: menuItem._id,
-//             name: menuItem.name,
-//             price: menuItem.price,
-//             quantity: 1,
-//           },
-//         ];
-//       }
-
-//       // Save updated cart items to session storage
-//       sessionStorage.setItem(
-//         `cartItems-${restaurantId}`,
-//         JSON.stringify(updatedCartItems)
-//       );
-
-//       return updatedCartItems;
-//     });
-//   };
-
-//   // Remove item from cart**********
-//   // const removeFromCart = (menuItem: MenuItemType) => {
-
-//   //   setCartItems((prevItems) => {
-//   //     const updatedItems = prevItems.filter(
-//   //       (item) => item._id !== menuItem._id
-//   //     );
-
-//   //     // Save updated cart items to session storage
-//   //     sessionStorage.setItem(
-//   //       `cartItems-${restaurantId}`,
-//   //       JSON.stringify(updatedItems)
-//   //     );
-
-//   //     return updatedItems;
-//   //   });
-//   // };
-
-//   const removeFromCart = (cartItem: CartItem) => {
-//     setCartItems((prevItems) => {
-//       const updatedItems = prevItems.filter(
-//         (item) => item._id !== cartItem._id
-//       );
-
-//       // Save updated cart items to session storage
-//       sessionStorage.setItem(
-//         `cartItems-${restaurantId}`,
-//         JSON.stringify(updatedItems)
-//       );
-
-//       return updatedItems;
-//     });
-//   };
-
-//   //Checkout
-
-//   const onCheckout = async (userFormData: UserFormData) => {
-//     if (!restaurant) {
-//       return;
-//     }
-
-//     //Create the cartItem object to send the API
-//     // **************
-//     const checkoutData = {
-//       cartItems: cartItems.map((cartItem) => ({
-//         menuItemId: cartItem._id,
-//         name: cartItem.name,
-//         quantity: cartItem.quantity.toString(),
-//       })),
-//       restaurantId: restaurant._id,
-//       deliveryDetails: {
-//         name: userFormData.name,
-//         addressLine1: userFormData.addressLine1,
-//         city: userFormData.city,
-//         country: userFormData.country,
-//         email: userFormData.email as string,
-//       },
-//     };
-
-//     const data = await createCheckoutSession(checkoutData);
-//     //Redirect to the url that stripe return
-//     if (data && data.url) {
-//       // Redirect to the URL that Stripe returns
-//       window.location.href = data.url;
-//     } else {
-//       // Handle error case when URL is not present
-//       toast.error("Error: Checkout session URL not found.");
-//     }
-//   };
-
-//   return (
-//     <div className="flex flex-col gap-10 my-8">
-//       {/* Header Image */}
-//       <AspectRatio ratio={16 / 5}>
-//         <img
-//           src={restaurant?.imageUrl}
-//           className="rounded-md object-cover h-full w-full"
-//         />
-//       </AspectRatio>
-//       {/* Menu Items and checkout grid */}
-//       <div className="grid md:grid-cols-[4fr_2fr] gap-5">
-//         {/* Left side menu item */}
-//         <div>
-//           {/* Restaurant header info */}
-//           <RestaurantInfo restaurant={restaurant} />
-
-//           {/* Restaurant Item list */}
-//           <div className="text-2xl font-bold tracking-tight py-6">Menu</div>
-
-//           {restaurant?.menuItems.map((menuItem) => (
-//             <MenuItem
-//               key={menuItem._id}
-//               menuItem={menuItem}
-//               addToCart={() => addToCart(menuItem)}
-//             />
-//           ))}
-//         </div>
-
-//         {/* Right side checkout card */}
-//         <div>
-//           <Card>
-//             {/* Order summary  */}
-//             <OrderSummary
-//               restaurant={restaurant}
-//               cartItems={cartItems}
-//               removeFromCart={removeFromCart}
-//             />
-
-//             {/* Order footer button  */}
-//             <CardFooter>
-//               <CheckoutButton
-//                 disabled={cartItems.length == 0}
-//                 onCheckout={onCheckout}
-//                 isLoading={isCheckoutLoading}
-//               />
-//             </CardFooter>
-//           </Card>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 function DetailPage() {
+  //Get the restaurantId from parameter
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+
   const { createCheckoutSession, isLoading: isCheckoutLoading } =
     useCreateCheckoutSession();
 
@@ -222,13 +41,16 @@ function DetailPage() {
     );
   }
 
+  // Add item to cart
   const addToCart = (menuItem: MenuItemType) => {
     setCartItems((prevCartItems) => {
+      // Check if the item already exists in the cart
       const existingCartItem = prevCartItems.find(
         (cartItem) => cartItem._id === menuItem._id
       );
       let updatedCartItems;
 
+      // If the item is already in the cart, increase the quantity
       if (existingCartItem) {
         updatedCartItems = prevCartItems.map((cartItem) =>
           cartItem._id === existingCartItem._id
@@ -236,6 +58,7 @@ function DetailPage() {
             : cartItem
         );
       } else {
+        // If the item is not in the cart, add the item with quantity 1
         updatedCartItems = [
           ...prevCartItems,
           {
@@ -247,6 +70,7 @@ function DetailPage() {
         ];
       }
 
+      // Save updated cart items to session storage
       sessionStorage.setItem(
         `cartItems-${restaurantId}`,
         JSON.stringify(updatedCartItems)
@@ -256,12 +80,14 @@ function DetailPage() {
     });
   };
 
-  const removeFromCart = (cartItem: CartItem) => {
+  // Remove item from cart
+  const removeFromCart = (menuItem: MenuItemType) => {
     setCartItems((prevItems) => {
       const updatedItems = prevItems.filter(
-        (item) => item._id !== cartItem._id
+        (item) => item._id !== menuItem._id
       );
 
+      // Save updated cart items to session storage
       sessionStorage.setItem(
         `cartItems-${restaurantId}`,
         JSON.stringify(updatedItems)
@@ -271,11 +97,14 @@ function DetailPage() {
     });
   };
 
+  //Checkout
+
   const onCheckout = async (userFormData: UserFormData) => {
     if (!restaurant) {
       return;
     }
 
+    //Create the cartItem object to send the API
     const checkoutData = {
       cartItems: cartItems.map((cartItem) => ({
         menuItemId: cartItem._id,
@@ -293,25 +122,35 @@ function DetailPage() {
     };
 
     const data = await createCheckoutSession(checkoutData);
+    //Redirect to the url that stripe return
     if (data && data.url) {
+      // Redirect to the URL that Stripe returns
       window.location.href = data.url;
     } else {
+      // Handle error case when URL is not present
       toast.error("Error: Checkout session URL not found.");
     }
   };
 
   return (
     <div className="flex flex-col gap-10 my-8">
+      {/* Header Image */}
       <AspectRatio ratio={16 / 5}>
         <img
           src={restaurant?.imageUrl}
           className="rounded-md object-cover h-full w-full"
         />
       </AspectRatio>
+      {/* Menu Items and checkout grid */}
       <div className="grid md:grid-cols-[4fr_2fr] gap-5">
+        {/* Left side menu item */}
         <div>
+          {/* Restaurant header info */}
           <RestaurantInfo restaurant={restaurant} />
+
+          {/* Restaurant Item list */}
           <div className="text-2xl font-bold tracking-tight py-6">Menu</div>
+
           {restaurant?.menuItems.map((menuItem) => (
             <MenuItem
               key={menuItem._id}
@@ -320,16 +159,20 @@ function DetailPage() {
             />
           ))}
         </div>
+
+        {/* Right side checkout card */}
         <div>
           <Card>
+            {/* Order summary  */}
             <OrderSummary
               restaurant={restaurant}
               cartItems={cartItems}
               removeFromCart={removeFromCart}
             />
+            {/* Order footer button  */}
             <CardFooter>
               <CheckoutButton
-                disabled={cartItems.length === 0}
+                disabled={cartItems.length == 0}
                 onCheckout={onCheckout}
                 isLoading={isCheckoutLoading}
               />
